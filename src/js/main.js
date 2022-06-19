5b5de1ca3e22e27bd33f7a6e08aa4cc07be228d1
CurvyLantern <ashfaqnasim33@gmail.com>
2022-06-19 18:38:40 +0600

    changed hover state

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ const ratingCard = getElById('ratingCard')
 const thankYouCard = getElById('thankYouCard')
 const insertRating = getElById('insertRating')
 const ratingContainer = getElById('ratingContainer')
+const allRatingButtons = [...ratingContainer.children]
 const submitBtn = getElById('sumbitBtn')
 
 ratingContainer.addEventListener('click', onRateFn)
@@ -16,12 +17,22 @@ function onRateFn(e) {
     if (e.target.tagName.toLowerCase() !== 'button') {
         return
     }
+    allRatingButtons.forEach((el) => {
+        if (el === e.target) return
+        el.classList.remove('bg-slate-400')
+        el.classList.remove('text-gray-50')
+    })
+    e.target.classList.add('bg-slate-400')
+    e.target.classList.add('text-gray-50')
     ratingStore.value = e.target.value
 }
 
 submitBtn.addEventListener('click', onSubmitFn)
 function onSubmitFn(e) {
     insertRating.textContent = ratingStore.value
+        ? ratingStore.value
+        : 'nothing'
+    ratingStore.value = undefined
 
     ratingCard.classList.add('rating__card--is-not-active')
     thankYouCard.classList.remove(
